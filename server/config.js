fc720a97494ffb0d5578b52c6d9aa5e70d1f2b44
Supimi Piumika <supimi.15@cse.mrt.ac.lk>
2018-05-08 22:07:02 +0530

    few changes

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   "database" : "mongodb://admin:1234@ds117070.mlab.com:17070/qsolver",
-  "port" : process.env.PORT || '3000',
+  "port" : process.env.PORT || '8080',
   "secretKey" : "supimi"
 }
